640173d4b53c72b6c3efb259d9d414d86eb7a7ce
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2023-07-27 08:54:49 -0300

    Update globals.sh

--- util/globals.sh
+++ util/globals.sh
@@ -1,3 +1,4 @@
-USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+USER=$SUDO_USER
+USER_HOME=$(getent passwd $USER | cut -d: -f6)
 TMP_DIRECTORY="$USER_HOME/.env.tmp"
 ENV_DIRECTORY="$USER_HOME/.env"
