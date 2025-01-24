3fba9702689f4982a67f6c419daf223728d0923b
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2025-01-24 13:19:42 -0300

    feat: VS Code and WSL, update on NVIM config

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -1,8 +1,74 @@
 local plugins = {
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false
-  },
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+    },
+    {
+        "alexghergh/nvim-tmux-navigation",
+        lazy = false
+    },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" }
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        event = "VeryLazy",
+        opts = function()
+            return require "custom.configs.null-ls"
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.configs.lspconfig"
+        end
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "lua-language-server",
+                "typescript-language-server",
+                "tailwindcss-language-server",
+                "eslint-lsp",
+                "prettierd"
+            }
+        }
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        ft = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "html",
+            config = function ()
+                require("nvim-ts-autotag").setup()
+            end
+        }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function()
+            local opts = require("plugins.configs.treesitter")
+            opts.ensure_installed = {
+                "lua",
+                "javascript",
+                "typescript",
+                "tsx",
+                "css",
+                "markdown",
+                "html",
+                "json"
+            }
+
+            return opts
+        end
+    }
 }
 
 return plugins
